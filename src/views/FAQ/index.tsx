8bd57eb691c7a5568c87de175c0f9bcc55afcbc8
github username <github email address>
2022-08-05 17:39:10 -0600

    break out into styled component file

--- src/views/FAQ/index.tsx
+++ src/views/FAQ/index.tsx
@@ -1,10 +1,17 @@
 /** @format */
 
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import Accordian from '../../components/Accordian';
 import { Questions } from './utils/Questions';
 import UnderConstruction from '../../components/UnderConstruction';
+import {
+	Container,
+	ScrollContainer,
+	ButtonContainer,
+	ScrollButton,
+	Accent,
+	ScrollToTopContainer,
+} from './styled-components';
 
 export default function FAQ() {
 	const displayAnswer = (index) => {
@@ -18,9 +25,6 @@ export default function FAQ() {
 
 	const scrollToSection = (key) => {
 		if (document !== null) {
-			// const section = document.getElementById(key) as HTMLElement;
-			// section.scrollIntoView();
-
 			const yOffset = -200;
 			const element = document.getElementById(key) as HTMLElement;
 			const y =
@@ -30,115 +34,6 @@ export default function FAQ() {
 		}
 	};
 
-	const Container = styled.div`
-		width: 100%;
-		max-width: 1000px;
-		padding: 8rem 0rem;
-		margin: auto;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: column;
-		h2 {
-			font-size: 30px;
-			font-family: 'Lexend', sans-serif;
-		}
-		h3 {
-			align-self: flex-start;
-			margin-left: 20px;
-		}
-	`;
-
-	const ScrollContainer = styled.div`
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		width: 100%;
-		max-width: 850px;
-		padding-bottom: 1rem;
-
-		@media only screen and (max-width: 900px) {
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			align-items: center;
-		}
-	`;
-
-	const ScrollButton = styled.div`
-		height: 150px;
-		width: 100%;
-		background-color: #000000;
-		color: #ffffff;
-		border-radius: 3px;
-		margin: 10px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		&:hover {
-			cursor: pointer;
-		}
-		&#bottom-button {
-			width: 150px;
-			height: 40px;
-			&:hover {
-				cursor: pointer;
-			}
-		}
-		&:hover + #accent {
-			opacity: 1;
-			width: 100%;
-		}
-
-		@media only screen and (max-width: 900px) {
-			height: 70px;
-			width: 90%;
-			border-radius: 2px;
-			margin: 2px;
-		}
-	`;
-
-	const Accent = styled.div`
-		height: 5px;
-		width: 100px;
-		opacity: 0;
-		&.lodging {
-			background-color: #3366ff;
-		}
-		&.rsvp {
-			background-color: #c9e265;
-		}
-		&.general {
-			background-color: #ff99ff;
-		}
-		&.weekend {
-			background-color: #3366ff;
-		}
-		&.wedding {
-			background-color: #c9e265;
-		}
-
-		@media only screen and (max-width: 900px) {
-			display: none;
-		}
-	`;
-
-	const ScrollToTopContainer = styled.div`
-		width: 100%;
-		display: flex;
-		flex-direction: column;
-		justify-content: flex-end;
-		align-items: flex-end;
-	`;
-
-	const ButtonContainer = styled.div`
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		width: 100%;
-		margin: 10px;
-	`;
 	return (
 		<>
 			{underConstruction ? (
---
+++ src/views/FAQ/styled-components/index.tsx
@@ -0,0 +1,113 @@
+/** @format */
+
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	width: 100%;
+	max-width: 1000px;
+	padding: 8rem 0rem;
+	margin: auto;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	h2 {
+		font-size: 30px;
+		font-family: 'Lexend', sans-serif;
+	}
+	h3 {
+		align-self: flex-start;
+		margin-left: 20px;
+	}
+`;
+
+export const ScrollContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	width: 100%;
+	max-width: 850px;
+	padding-bottom: 1rem;
+
+	@media only screen and (max-width: 900px) {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+`;
+
+export const ScrollButton = styled.div`
+	height: 150px;
+	width: 100%;
+	background-color: #000000;
+	color: #ffffff;
+	border-radius: 3px;
+	margin: 10px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	&:hover {
+		cursor: pointer;
+	}
+	&#bottom-button {
+		width: 150px;
+		height: 40px;
+		&:hover {
+			cursor: pointer;
+		}
+	}
+	&:hover + #accent {
+		opacity: 1;
+		width: 100%;
+	}
+
+	@media only screen and (max-width: 900px) {
+		height: 70px;
+		width: 90%;
+		border-radius: 2px;
+		margin: 2px;
+	}
+`;
+
+export const Accent = styled.div`
+	height: 5px;
+	width: 100px;
+	opacity: 0;
+	&.lodging {
+		background-color: #3366ff;
+	}
+	&.rsvp {
+		background-color: #c9e265;
+	}
+	&.general {
+		background-color: #ff99ff;
+	}
+	&.weekend {
+		background-color: #3366ff;
+	}
+	&.wedding {
+		background-color: #c9e265;
+	}
+
+	@media only screen and (max-width: 900px) {
+		display: none;
+	}
+`;
+
+export const ScrollToTopContainer = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+	align-items: flex-end;
+`;
+
+export const ButtonContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	margin: 10px;
+`;
